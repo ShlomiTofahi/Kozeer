b459ec5ebd3d8ec1a7d55c58f086b08ac19b587f
ShlomiTofahi <shlomitofahi@gmail.com>
2021-07-22 22:43:13 +0300

    updated over13 page

--- client/src/components/Over13.js
+++ client/src/components/Over13.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Button } from 'reactstrap';
 
 export default class About extends Component {
@@ -6,58 +6,55 @@ export default class About extends Component {
         path: "/images/header/"
     }
 
-
-    aboutStyle = () => {
-        return {
-            WebkitBorderRadius: '10px',
-            MozBorderRadius: '10px',
-            borderRadius: '10px',
-            margin: '0',
-            // position: 'absolute',
-            paddingTop: '10%',
-            backgroundColor: '#323232',
-            color: 'white',
-            // width: '50%',
-        };
-    };
     render() {
         return (
-            <div align='center' className="wrapper animated bounceInLeft" style={bgStyle}>
-                {/* <div style={this.aboutStyle()}> */}
+            <Fragment>
+                <div align='center' className="wrapper animated bounceInLeft" style={bgStyle}></div>
+                <div style={bodyStyle}>
                     <legend>
                         <h2 className='brand display-4 pt-4' style={window.innerWidth >= 992 ? { fontFamily: "'Shadows Into Light', Kimberly Geswein", opacity: '0.4' } : { fontFamily: "'Shadows Into Light', Kimberly Geswein", opacity: '0.4', fontSize: '2.0em' }}>
                             OVER 13?
                         </h2>
                     </legend>
                     <p className='lead p-5'>
-                        this site containe sensitive content
+                        This site contain sensitive content
                     </p>
                     <Button
+                        className='mb-4'
                         color='info'
                         size='sm'
                         onClick={this.props.onOver13Click}
                     >yes i'm over 13</Button>
-                {/* </div> */}
-            </div>
+                </div>
+            </Fragment>
         )
     }
 }
 
 const bgStyle = {
-    backgroundImage: `url(/images/main/bg.png)`,
-    // backgroundAttachment: 'fixed',
-    // backgroundSize: 'contain',
-    // backgroundPosition: 'bottom left',
-    // display:'absolute',
-    // minheight: '100%',
-    // minWidth: '1024px',
-      
-    /* Set up proportionate scaling */
-    width: '100%',
-    height: 'auto',
-      
-    /* Set up positioning */
-    position: 'fixed',
-    top: '0',
-    left: '0'
-  }
+    backgroundImage: `url(/images/main/13page.jpg)`,
+    paddingTop: "10%",
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
+    backgroundPosition: 'bottom left',
+    backgroundRepeat: 'no-repeat',
+    filter: 'blur(4px)',
+    height: '721px'
+}
+
+const bodyStyle = {
+    backgroundColor: 'rgb(0,0,0)',
+    backgroundColor: 'rgba(0,0,0, 0.4)',
+    color: 'white',
+    fontWeight: 'bold',
+    border: ' 3px solid #f1f1f1',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: ' translate(-50%, -50%)',
+    zIndex: '2',
+    width: '50%',
+    padding: '20px',
+    textAlign: 'center',
+    fontFamily: 'Arial, Helvetica, sans-serif'
+}
